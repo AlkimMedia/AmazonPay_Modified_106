3667962904325568b503fc5a6384c19a7c5c0441
mkreusch <info@kreusch-creative.de>
2020-12-10 21:16:11 +0100

    cleanup + merge with xtc 43a68d00a82edf78522736da204429e10d227e73 (less checkout redirects)

--- new_dirs/callback/amazon_pay/create_checkout_session.php
+++ new_dirs/callback/amazon_pay/create_checkout_session.php
@@ -3,8 +3,6 @@
 chdir('../../');
 
 require_once 'includes/application_top.php';
-error_reporting(E_ALL);
-ini_set('display_errors', true);
 require_once 'includes/modules/payment/amazon_pay/amazon_pay.php';
 
 $checkoutHelper = new \AlkimAmazonPay\CheckoutHelper();
