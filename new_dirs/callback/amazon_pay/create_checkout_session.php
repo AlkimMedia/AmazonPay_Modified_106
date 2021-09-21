220c6d9e227f89dc8948b149e25f39d08d043abd
mkreusch <info@kreusch-creative.de>
2021-09-21 02:14:02 +0200

    v1.1.0

--- new_dirs/callback/amazon_pay/create_checkout_session.php
+++ new_dirs/callback/amazon_pay/create_checkout_session.php
@@ -7,5 +7,4 @@ require_once 'includes/modules/payment/amazon_pay/amazon_pay.php';
 
 $checkoutHelper = new \AlkimAmazonPay\CheckoutHelper();
 $checkoutSession = $checkoutHelper->createCheckoutSession();
-
 echo json_encode(['amazonCheckoutSessionId'=>$checkoutSession->getCheckoutSessionId()]);
